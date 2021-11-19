4663067c1ef04b1ba411bfa23f38975fc8b99503
AntonAks <antonaks84@gmail.com>
2021-11-19 18:18:10 +0200

    finished with user model

--- bot/db_handler/__init__.py
+++ bot/db_handler/__init__.py
@@ -6,44 +6,70 @@ from datetime import datetime
 from pymongo import MongoClient
 
 client = MongoClient(settings.DB)
-db = client.main_db
 
 
 class Model(object):
+    db = client.main_db
+
     def __init__(self, **kwargs):
-        print(">>>", kwargs['collection'], kwargs['db'])
-        self.db = kwargs['db']
         self.collection = kwargs['collection']
 
-    def save(self, value):
-        message_dict = json.loads(str(value))
+    def save(self):
         self.db[self.collection].insert_one({
-            "message": message_dict
+            "item": self.item()
         })
+
+    def __str__(self):
+        return str(self.item())
+
+    def item(self):
+        return {i.replace(f"_{self.collection}__", ""): self.__dict__[i] for i in self.__dict__
+                if i.startswith(f"_{self.collection}")}
 
 
 class User(Model):
 
-    def __init__(self, **kwargs):
-        self._name = None
-        self._timezone =
-        super().__init__(collection=__class__.__name__, db=db)
+    def __init__(self):
+        self.__id = None
+        self.__timezone = str(pytz.timezone('Europe/Kiev'))
+        super().__init__(collection=__class__.__name__)
 
     @property
-    def name(self):
-        return self._name
+    def id(self):
+        return self.__id
 
-    @name.setter
-    def name(self, value):
-        self._name = value
+    @id.setter
+    def id(self, value):
+        self.__id = value
 
     @property
     def timezone(self):
-        return self._timezone
+        return self.__timezone
 
     @timezone.setter
     def timezone(self, value):
-        self._timezone = value
+        self.__timezone = value
+
+    def is_user_exist(self):
+        check = Model.db[__class__.__name__].find_one({"item.id": self.__id})
+        if check is None:
+            return False
+        return True
+
+    def save(self):
+        if self.is_user_exist():
+            logging.warning(f"User is already exist")
+        else:
+            logging.warning(f"New user saved")
+            super(User, self).save()
+
+    @classmethod
+    def get_all_users(cls):
+        return list(cls.db[__class__.__name__].find())
+
+    @classmethod
+    def drop_collection(cls):
+        cls.db[__class__.__name__].drop()
 
 
 class Word(Model):
@@ -51,8 +77,7 @@ class Word(Model):
 
 
 class Message(Model):
-    def __init__(self, **kwargs):
-        # collection = db[__class__.__name__]
+    def __init__(self):
         collection = __class__.__name__
-        super().__init__(collection=collection, db=db)
+        super().__init__(collection=collection)
 
--- bot/handlers.py
+++ bot/handlers.py
@@ -1,11 +1,34 @@
 from aiogram import types
 from loader import dp
-from db_handler import Message
+from db_handler import Message, User
+
+
+@dp.message_handler(commands=['start'])
+async def _start_command(message: types.Message):
+    user = User()
+    user.id = message.from_user.id
+    try:
+        user.user_name = message.from_user.username
+    except Exception:
+        user.user_name = None
+    user.save()
+    await message.answer(f"Пользователь сохранен! {user}")
+
+
+@dp.message_handler(commands=['get_users'])
+async def _get_users(message: types.Message):
+    await message.answer(f"{User.get_all_users()}")
+
+
+@dp.message_handler(commands=['del_users'])
+async def _get_users(message: types.Message):
+    User.drop_collection()
+    await message.answer(f"Пользователи удалены...")
 
 
 # COMMON MESSAGE HANDLER
 @dp.message_handler()
 async def echo_message(message: types.Message):
     m = Message()
-    m.save(message)
+    m.save()
     await message.answer(f"echo answer: {message['text']}")
