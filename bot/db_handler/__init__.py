549d0082c254af33f6525c5eb0fd10f53f126b7e
AntonAks <antonaks84@gmail.com>
2021-11-19 10:00:09 +0200

    refactored DB models

--- bot/db_handler/__init__.py
+++ bot/db_handler/__init__.py
@@ -4,20 +4,49 @@ import pytz
 import settings
 from datetime import datetime
 from pymongo import MongoClient
-from . mongo_model import Model
-
-
-default_tz = str(pytz.timezone('Europe/Kiev'))
 
 client = MongoClient(settings.DB)
 db = client.main_db
 
 
-class User:
-    pass
+class Model(object):
+    def __init__(self, **kwargs):
+        print(">>>", kwargs['collection'], kwargs['db'])
+        self.db = kwargs['db']
+        self.collection = kwargs['collection']
+
+    def save(self, value):
+        message_dict = json.loads(str(value))
+        self.db[self.collection].insert_one({
+            "message": message_dict
+        })
 
 
-class Word:
+class User(Model):
+
+    def __init__(self, **kwargs):
+        self._name = None
+        self._timezone =
+        super().__init__(collection=__class__.__name__, db=db)
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        self._name = value
+
+    @property
+    def timezone(self):
+        return self._timezone
+
+    @timezone.setter
+    def timezone(self, value):
+        self._timezone = value
+
+
+class Word(Model):
     pass
 
 
@@ -27,6 +56,3 @@ class Message(Model):
         collection = __class__.__name__
         super().__init__(collection=collection, db=db)
 
-
-if __name__ == '__main__':
-    m = Message()
